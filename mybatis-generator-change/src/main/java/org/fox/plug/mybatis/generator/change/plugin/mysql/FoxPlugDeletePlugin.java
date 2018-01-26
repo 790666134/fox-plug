458c4c5887be24d855d8da6b910cc717b68563db
miaoxy <miaoxinyang@i-guan.com>
2018-01-26 14:54:01 +0800

    加入对mybatis-generator的相关配置

---
+++ mybatis-generator-change/src/main/java/org/fox/plug/mybatis/generator/change/plugin/mysql/FoxPlugDeletePlugin.java
@@ -0,0 +1,23 @@
+package org.fox.plug.mybatis.generator.change.plugin.mysql;
+
+import java.util.List;
+
+import org.mybatis.generator.api.PluginAdapter;
+
+/**
+ * FoxPlug Delete Plugin 生成Delete Sql ,支持Mysql
+ * @ClassName: FoxPlugDeletePlugin  
+ * @Description: FoxPlugDeletePlugin 自定义FoxPlug控件，实现Delete方法
+ * @author yang  
+ * @date 2018年1月26日  
+ *
+ */
+public class FoxPlugDeletePlugin extends PluginAdapter{
+
+	@Override
+	public boolean validate(List<String> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
